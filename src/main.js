cab00b33ddcebd402b95c939cd83386e9c82dbec
吴胜业 <betterwusy@163.com>
2023-04-04 17:03:49 +0800

    处理样式资源

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import add from "./js/add";
+import sum from "./js/sum";
+
+import './styles/css/index.css'
+import './styles/less/index.less'
+import './styles/scss/index.scss'
+
+console.log(add(1, 3));
+console.log(sum(1, 2, 3, 4));
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  // clean: true,
+  entry: "./src/main.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.less$/,
+        use: ["style-loader", "css-loader", "less-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [],
+};
