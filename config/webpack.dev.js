ec16667285d646f40eb256e5e3c4469780b0de2c
吴胜业 <betterwusy@163.com>
2023-04-05 21:44:03 +0800

    add: 配置 oneOf

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -22,60 +22,64 @@ module.exports = {
   },
   module: {
     rules: [
-      /**使用 style-loader、css-loader 处理样式资源
-       * 以及 css 预处理器 [sass|less]-loader
-       */
       {
-        test: /\.css$/,
-        use: getStyleLoaders(),
-      },
-      {
-        test: /\.less$/,
-        use: getStyleLoaders("less-loader"),
-      },
-      {
-        test: /\.s(a|c)ss$/,
-        use: getStyleLoaders("sass-loader"),
-      },
-
-      /**
-       * 使用 webpack5 内置 asset module（资源模块）处理资源文件
-       * 如：图片、字体文件、音/视频 等
-       * 可选资源模块类型如下：
-       *  - asset/resource: 将文件导出到输出目录中
-       *  - asset/inline:   将文件转成 dataUrl
-       *  - asset/source:   导出文件的源代码注入到输出文件中
-       *  - asset:  通用资源类型, 按照默认条件，自动地在 resource 和 inline 之间进行选择
-       *             默认是小于 8kb 用 inline 方式，否则用 resource 方式
-       *             用 parser.dataUrlCondition.maxSize 属性来修改此条件（单位 b）
-       */
-      {
-        test: /\.(p(n|e)g|jpe?g|gif|webp)$/,
-        type: "asset",
-        parser: {
-          dataUrlCondition: {
-            maxSize: 10 * 1024,
+        oneOf: [
+          /**使用 style-loader、css-loader 处理样式资源
+           * 以及 css 预处理器 [sass|less]-loader
+           */
+          {
+            test: /\.css$/,
+            use: getStyleLoaders(),
           },
-        },
-        // generator 对象配置生成的文件路径和名称
-        generator: {
-          filename: "static/images/[hash:8][ext][query]",
-        },
-      },
-      // 处理字体资源
-      {
-        test: /\.(ttf|woff2?|mp3|mp4|avi)$/,
-        type: "asset/resource",
-        generator: {
-          filename: "static/media/[hash:8][ext][query]",
-        },
-      },
+          {
+            test: /\.less$/,
+            use: getStyleLoaders("less-loader"),
+          },
+          {
+            test: /\.s(a|c)ss$/,
+            use: getStyleLoaders("sass-loader"),
+          },
 
-      // 对 js 文件使用 babel 进行处理
-      {
-        test: /\.js$/,
-        exclude: /node_modules/, // 排除node_modules代码不编译
-        loader: "babel-loader",
+          /**
+           * 使用 webpack5 内置 asset module（资源模块）处理资源文件
+           * 如：图片、字体文件、音/视频 等
+           * 可选资源模块类型如下：
+           *  - asset/resource: 将文件导出到输出目录中
+           *  - asset/inline:   将文件转成 dataUrl
+           *  - asset/source:   导出文件的源代码注入到输出文件中
+           *  - asset:  通用资源类型, 按照默认条件，自动地在 resource 和 inline 之间进行选择
+           *             默认是小于 8kb 用 inline 方式，否则用 resource 方式
+           *             用 parser.dataUrlCondition.maxSize 属性来修改此条件（单位 b）
+           */
+          {
+            test: /\.(p(n|e)g|jpe?g|gif|webp)$/,
+            type: "asset",
+            parser: {
+              dataUrlCondition: {
+                maxSize: 10 * 1024,
+              },
+            },
+            // generator 对象配置生成的文件路径和名称
+            generator: {
+              filename: "static/images/[hash:8][ext][query]",
+            },
+          },
+          // 处理字体资源
+          {
+            test: /\.(ttf|woff2?|mp3|mp4|avi)$/,
+            type: "asset/resource",
+            generator: {
+              filename: "static/media/[hash:8][ext][query]",
+            },
+          },
+
+          // 对 js 文件使用 babel 进行处理
+          {
+            test: /\.js$/,
+            exclude: /node_modules/, // 排除node_modules代码不编译
+            loader: "babel-loader",
+          },
+        ],
       },
     ],
   },
@@ -111,5 +115,5 @@ module.exports = {
     open: true,
   },
 
-  devtool: 'cheap-module-source-map',
+  devtool: "cheap-module-source-map",
 };
